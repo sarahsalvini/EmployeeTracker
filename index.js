1b819d939834ff3abd26c97235706bb01656c67c
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-03-04 11:58:07 -0600

    created database and linked to js file

---
+++ index.js
@@ -0,0 +1,85 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Marileheide8!",
+  database: "employee_DB"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("connection id", connection.threadId);
+    menu();
+  });
+
+
+function options() {
+    inquirer
+      .prompt({
+        name: "optionList",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View All employees by deparment",
+            "View all employees by manager",
+            "Add employee",
+            "Remove employee",
+            "Update employee role",
+            "Update employee manager",
+            "View all roles",
+            "Add role",
+            "Remove role"
+        ]
+      })
+      .then(function(answer) {
+          console.log(answer.optionList)
+        switch (answer.optionList) {
+        case "View all employees":
+          viewAllEmployees();
+          break;
+  
+        case "View All employees by deparment":
+          
+          break;
+  
+        case "View all employees by manager":
+          
+          break;
+  
+        case "Add employee":
+          
+          break;
+  
+        case "Remove employee":
+          
+          break;
+
+        case "Update employee role":
+          
+          break;
+
+        case "Update employee manager":
+          
+          break;
+  
+        case "View all roles":
+          
+          break;
+
+        case "Add role":
+          
+          break;
+
+        case "Remove role":
+          
+          break;
+        }
+      });
+  }
