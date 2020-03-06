2a0b41aeac2ad67722de5a055cc11d7d1b437c99
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-03-06 13:44:56 -0600

    need to fix view by department

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
 
+const departments = ["Sales", "Engineering", "Finance", "Legal"];
+const roles = ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Accountant", "Legal Team Lead", "Lawyer"];
+const managers = ["Emma Smith", "Bobby Hayes", "Lisa Brown", "Sam Taylor"]
+
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -34,8 +39,7 @@ function options() {
                 "Update employee manager",
                 "Add employee",
                 "Add role",
-                "Remove role",
-                "Remove employee"
+                "Add department",
             ]
         })
         .then(function (answer) {
@@ -58,28 +62,21 @@ function options() {
                     break;
 
                 case "Update employee role":
-
-                    break;
-
-                case "Update employee manager":
-
+                    upEmployRole();
                     break;
 
                 case "Add employee":
-            
+                    addEmploy();
                     break;
 
                 case "Add role":
+                    addRole();
+                    break;
 
+                case "Add department":
+                    addDepart();
                     break;
                 
-                case "Remove employee":
-
-                    break;
-
-                case "Remove role":
-
-                    break;
             }
         });
 }
@@ -104,7 +101,8 @@ function employeesDepart() {
         choices: ["Sales",
             "Engineering",
             "Finance",
-            "Legal"
+            "Legal", 
+            "HR"
         ]
     }).then(function (answer) {
         const department = answer.departments;
@@ -135,5 +133,193 @@ function viewRoles(){
         if (err) throw err;
         console.table(res)
         options()
+    })
+}
+
+function upEmployRole(){
+    connection.query("SELECT id, first_name, last_name FROM employee", function(err, result) {
+    if (err) throw err;
+    const employeeName = [];
+    for (let i = 0; i < result.length; i++) {
+        const choices = (result[i].first_name + " " + result[i].last_name);
+        employeeName.push(choices);
+    }
+    inquirer.prompt([{
+                name: "employee",
+                type: "list",
+                message: "Which employee would you like to update?",
+                choices: employeeName
+            },
+            {
+                name: "newTitle",
+                type: "list",
+                message: "What is the employee's new role?",
+                choices: ["Sales Lead",
+                    "Salesperson",
+                    "Lead Engineer",
+                    "Software Engineer",
+                    "Accountant",
+                    "Legal Team Lead",
+                    "Lawyer",
+                    "Lead Engineer"
+                ]
+            }
+        ])
+        .then(function(answer) {
+            const employeeName = answer.employee;
+            if (answer.newTitle === "Sales Lead") {
+                var roleID = "1";
+            } else if (answer.newTitle === "Salesperson") {
+                var roleID = "2";
+            } else if (answer.newTitle === "Lead Engineer") {
+                var roleID = "3";
+            } else if (answer.newTitle === "Software Engineer") {
+                var roleID = "4";
+            } else if (answer.newTitle === "Accountant") {
+                var roleID = "5";
+            } else if (answer.newTitle === "Legal Team Lead") {
+                var roleID = "6";
+            } else if (answer.newTitle === "Lawyer") {
+                var roleID = "7";
+            } else if (answer.newTitle === "Lead Engineer") {
+                var roleID = "8";
+            }
+            connection.query(`UPDATE employee SET ? WHERE ?`, [{
+                        role_id: answer.roleID
+                    },
+                    {
+                        id: chosenItem.id
+                    }
+                ],
+                function(err, ) {
+                    if (err) throw err;
+
+                })
+        })
+})
+}
+
+function addEmploy() {
+    inquirer.prompt([{
+        name: "first",
+        type: "input",
+        message: "What is the employee's first name?"
+    },
+    {
+        name: "last",
+        type: "input",
+        message: "What is the employee's last name?"
+    },
+    {
+        name: "role",
+        type: "list",
+        message: "What is the employee's role?",
+        choices: roles
+    },
+    {
+        name: "man",
+        type: "list",
+        message: "Who is the employee's manager?",
+        choices: managers
+    }
+]).then(function(answer) {
+    if (answer.role === "Sales Lead") {
+        var roleid = "5"
+    } else if (answer.role === "Salesperson") {
+        var roleid = "6"
+    } else if (answer.role === "Lead Engineer") {
+        var roleid = "7"
+    } else if (answer.role === "Software Engineer") {
+        var roleid = "8"
+    } else if (answer.role === "Accountant") {
+        var roleid = "9"
+    } else if (answer.role === "Legal Team Lead") {
+        var roleid = "10"
+    } else if (answer.role === "Lawyer") {
+        var roleid = "11"
+    };
+    if (answer.man === "Ashley Rodriguez") {
+        var manid = "3"
+    } else if (answer.man === "John Doe") {
+        var manid = "1"
+    } else if (answer.man === "Sarah Lourd") {
+        var manid = "6"
+    } else if (answer.man === "Mike Chan") {
+        var manid = "2"
+    }
+    connection.query(
+        `INSERT INTO employee SET ?`, {
+            first_name: answer.first,
+            last_name: answer.last,
+            role_id: roleid,
+            manager_id: manid,
+        },
+        function(err) {
+            if (err) throw err;
+            console.log("Employee Added Successfully");
+            viewAllEmployees();
+        }
+    )
+})
+}
+
+function addRole(){
+    inquirer.prompt([{
+        name: "role",
+        type: "input",
+        message: "Which role would you like to add?"
+    }, {
+        name: "dept",
+        type: "list",
+        message: "Into which department would you like to assign this role?",
+        choices: departments
+    }, {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for this role?",
+    }])
+    .then(function(answer) {
+        if (answer.dept === "Sales") {
+            var depID = "1"
+        } else if (answer.dept === "Engineering") {
+            var depID = "2"
+        } else if (answer.dept === "Finance") {
+            var depID = "3"
+        } else if (answer.dept === "Legal") {
+            var depID = "4"
+        }
+        connection.query(
+            `INSERT INTO role SET ?`, {
+                title: answer.role,
+                salary: answer.salary,
+                department_id: depID
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Role Created Successfully");
+                viewRoles();
+            }
+        );
+
+    })
+}
+
+function addDepart(){
+    inquirer.prompt([{
+        name: "dep",
+        type: "input",
+        message: "Which department would you like to add?"
+    }])
+    .then(function(answer) {
+        connection.query(
+            `INSERT INTO department SET ?`, {
+                name: answer.dep,
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Department Created Successfully");
+                departs()
+            }
+        )
     })
 }
--- schemas.sql
+++ schemas.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE department(
-	id int NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
 	name varchar(30) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-	id int NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
 	title varchar(30) NOT NULL,
     salary decimal(10, 3),
     department_id INT NOT NULL,
